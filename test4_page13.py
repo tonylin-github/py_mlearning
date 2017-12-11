fe32f8388b677cbf14da61ffb9c054d58e62d642
tonylin <sonic_magic_2006@hotmail.com>
2017-12-11 23:25:23 +0800

    20171211

---
+++ test4_page13.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+%matplotlib inline
+import numpy as np
+
+PATH=r'/home/admin/python_base/'
+r=requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/\
+iris.data')
+with open(PATH+'iris.data','w') as f:
+    f.write(r.text)
+
+os.chdir(PATH)
+cur_dir=os.getcwd()
+#print("cur_dir=%s"%(cur_dir))                                                  
+df=pd.read_csv('/home/admin/python_base/iris.data',names=['sepal length','sepal\
+ width','petal length','petal width','class'])
+#print(df.corr(method="kendall"))
+fig,ax=plt.subplots(2,2,figsize=(6,4))
+ax[0][0].hist(df['petal width'],color='black')
+ax[0][0].set_ylabel('count',fontsize=12)
+ax[0][0].set_xlabel('width',fontsize=12)
+ax[0][0].set_title('Iris Petal Width',fontsize=14,y=1.01)
+
+
+ax[0][1].hist(df['petal length'],color='black')
+ax[0][1].set_ylabel('count',fontsize=12)
+ax[0][1].set_xlabel('width',fontsize=12)
+ax[0][1].set_title('Iris Petal length',fontsize=14,y=1.01)
+
+
+ax[1][0].hist(df['sepal width'],color='black')
+ax[1][0].set_ylabel('count',fontsize=12)
+ax[1][0].set_xlabel('width',fontsize=12)
+ax[1][0].set_title('Iris sepal Width',fontsize=14,y=1.01)
+
+ax[1][1].hist(df['sepal length'],color='black')
+ax[1][1].set_ylabel('count',fontsize=12)
+ax[1][1].set_xlabel('width',fontsize=12)
+ax[1][1].set_title('Iris sepal length',fontsize=14,y=1.01)
+
+fig.savefig("20171211v2.png")
+
+
+
